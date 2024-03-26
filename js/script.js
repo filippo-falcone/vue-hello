f0bb592bbbdb1bd3b2b0131c35494da469a1e507
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-26 14:46:21 +0100

    feat: print message on html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+/*
+// Stampo a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
+
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
+            message: 'Hello 122!'
         }
     }
 }).mount('#app')
